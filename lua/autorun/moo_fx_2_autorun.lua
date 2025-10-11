6c91bdf54ad2a106fbb64a511a548908f56605e0
Yurisakisoba <wimwaver.yurifish@gmail.com>
2025-10-11 19:09:10 -0300

    Moo's Content Pack 1 Fetch
    - Post nuke fetch

---
+++ lua/autorun/moo_fx_2_autorun.lua
@@ -0,0 +1,26 @@
+--I don't know if theres a limit to how many particles can be precached per autorun, but I think there MIGHT be?
+game.AddParticles("particles/moo_misc_fx.pcf")
+game.AddParticles("particles/mysterybox.pcf")
+
+PrecacheParticleSystem("mysterybox_beam")
+PrecacheParticleSystem("mysterybox_roll")
+
+PrecacheParticleSystem("tomb_box_aura")
+PrecacheParticleSystem("tomb_box_open_aura")
+
+PrecacheParticleSystem("zmb_iw_spawn_portal")
+
+PrecacheParticleSystem("zmb_exo_blinker_aura")
+PrecacheParticleSystem("zmb_exo_emp_aura")
+PrecacheParticleSystem("zmb_exo_fire_aura")
+
+PrecacheParticleSystem("zmb_radz_aura")
+PrecacheParticleSystem("zmb_radz_split")
+PrecacheParticleSystem("zmb_radz_split_explo")
+PrecacheParticleSystem("zmb_radz_bomb_aoe")
+PrecacheParticleSystem("zmb_radz_bomb_aura")
+PrecacheParticleSystem("zmb_radz_bomb_charge")
+PrecacheParticleSystem("zmb_radz_blast_inhale")
+PrecacheParticleSystem("zmb_radz_blast_exhale")
+
+PrecacheParticleSystem("zmb_dog_fire_breath")
